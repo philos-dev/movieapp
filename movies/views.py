7072079dbd5836f8e26cacb552c122dc78ad270a
Patrick Torhorst <ptorhorst@questce.com>
2024-07-25 11:43:11 -0500

    init command

---
+++ movies/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    context = {
+        'movies': ['matrix', 'john wick', 'kung fu panda', 'lilo and stitch']
+    }
+    return render(request, 'movies/index.html', context)
+
+def about(request):
+    return render(request, 'movies/about.html', {})
